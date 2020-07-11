51ef5cbc95bbe9c71128524751a21b90dc728405
tanvird3 <tanvir.d3@gmail.com>
2020-07-11 15:17:51 +0600

    Design Update

--- app.py
+++ app.py
@@ -197,6 +197,7 @@ app.layout = html.Div(
         "backgroundColor": colors["background"],
         "horizontalAlign": "middle",
         "width": "100%",
+        "height": "100%",
         "align-items": "center",
         "justify-content": "center",
     },
